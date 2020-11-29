0181a42e71b93136f7d3ad52188bff2fe3415d37
AlvinOng <alvinongzx@gmail.com>
2020-11-30 00:12:55 +0800

    Add new function for random score.

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -3,11 +3,16 @@ CP1404 2020 Practical 1
 Student Name: Alvin Ong Zhi Xiang
 Fixing broken program to determine score status
 """
+import random
 
 
 def main():
 
     score = user_score()
+    output_result(score)
+
+
+def output_result(score):
     if score < 0 or score > 100:
         print("Invalid score")
     elif score >= 90:
@@ -22,5 +27,9 @@ def user_score():
     score = float(input("Enter score:"))
     return score
 
+
+def random_score():
+    score = random.randint(0, 101)
+    return score
 
 main()
