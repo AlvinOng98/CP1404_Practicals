c038fac8cccdb687308af754573fe3e034a167c7
AlvinOng <alvinongzx@gmail.com>
2020-11-30 00:01:08 +0800

    Refactor conversion calculations into functions

--- Prac_03/temperatures.py
+++ Prac_03/temperatures.py
@@ -15,16 +15,26 @@ def main():
     while choice != "Q":
         if choice == "C":
             celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
+            fahrenheit = convert_celsius(celsius)
             print("Result: {:.2f} F".format(fahrenheit))
             break
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit(fahrenheit)
             print("Result: {:.2f} C".format(celsius))
             break
         else:
             print("Invalid option")
+
+
+def convert_fahrenheit(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    return celsius
+
+
+def convert_celsius(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
 
 
 print("Thank you.")
