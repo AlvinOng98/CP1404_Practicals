9131628ff5beb43ff256dee3de79bdbfae3239bc
AlvinOng <alvinongzx@gmail.com>
2020-12-06 23:16:32 +0800

    change months variable to number_of_months

--- Prac_04/total_income.py
+++ Prac_04/total_income.py
@@ -9,15 +9,15 @@ def main():
 
     """Display income report for incomes over a given number of months."""
     incomes = []
-    months = int(input("How many months? "))
+    number_of_months = int(input("How many months? "))
 
-    for number_of_months in range(1, months + 1):
+    for number_of_months in range(1, number_of_months + 1):
         income = float(input("Enter income for month {}:".format(number_of_months)))
         incomes.append(income)
 
     print("\nIncome Report\n-------------")
     total = 0
-    for number_of_months in range(1, months + 1):
+    for number_of_months in range(1, number_of_months + 1):
         income = incomes[number_of_months - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(number_of_months, income, total))
