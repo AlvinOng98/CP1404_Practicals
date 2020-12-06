244a9f6eeaecbbe1359d0c528c7b117c7f19bb12
AlvinOng <alvinongzx@gmail.com>
2020-12-07 02:15:35 +0800

    add Woefully inadequate security checker program

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -8,22 +8,30 @@ MAX_INPUT = 5
 
 
 def main():
-
     numbers = num_input()
     first = numbers[0]
     last = numbers[-1]
     smallest = min(numbers)
     largest = max(numbers)
-    average = sum(numbers)/MAX_INPUT
+    average = sum(numbers) / MAX_INPUT
     print("The first number is {}".format(first))
     print("The last number is {}".format(last))
     print("The smallest number is {}".format(smallest))
     print("The largest number is {}".format(largest))
     print("The average of the numbers is {}".format(average))
 
+    userNames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
+                 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
+                 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+    name = user_input()
+    answer = user_check(name, userNames)
+    if answer:
+        print("Access granted")
+    else:
+        print("Access denied")
+
 
 def num_input():
-
     numbers = []
     for i in range(0, MAX_INPUT):
         numInput = int(input("Please enter a number:"))
@@ -31,20 +39,13 @@ def num_input():
     return numbers
 
 
-userNames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45',
-             'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState',
-             'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
-
-
 def user_input():
-
     username = input("Please enter your username:")
 
     return username
 
 
 def user_check(username, userNames):
-
     if username in userNames:
         return True
     else:
